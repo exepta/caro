bed6b8c400000c09d4743c4f635b3a0fb62a8add
exepta <genshin.exepta@outlook.com>
2025-11-25 22:36:34 +0100

    feat(#23): frontend test coverage

--- apps/web/src/app/pages/client-view-page/friends-content/tabs/all-tab/all-tab.ts
+++ apps/web/src/app/pages/client-view-page/friends-content/tabs/all-tab/all-tab.ts
@@ -1,9 +1,15 @@
 import {Component, EventEmitter, Input, Output, signal} from '@angular/core';
 import {FriendVm} from '../../../../../services/friends.vm';
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {TooltipComponent} from "../../../../components/tooltip/tooltip.component";
+import {faCheck, faComment, faEnvelope, faPhone} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-all-tab',
-  imports: [],
+    imports: [
+        FaIconComponent,
+        TooltipComponent
+    ],
   templateUrl: './all-tab.html',
   styleUrls: ['./all-tab.scss', '../../../../../shared/styles/friends-list.scss']
 })
@@ -17,4 +23,7 @@ export class AllTab {
   onFriendAddClick() {
     this.friendAddClick.emit();
   }
+
+  protected readonly faPhone = faPhone;
+  protected readonly faComment = faComment;
 }
